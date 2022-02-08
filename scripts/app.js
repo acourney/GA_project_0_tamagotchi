8f12eb272a5ec05617a28729657ae2b015101d2e
acourney <acourney@ucsc.edu>
2022-02-07 18:41:53 -0800

    add ability to cycle through nav bar with the select button

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-console.log("tamagotchi loaded.");
 /* === Instantiate Tamagotchi === */
 class Tamagotchi {
   constructor() {
@@ -25,6 +24,7 @@ class Tamagotchi {
 
   increaseAge(){
     return this.age += 1;
+    // this will change the image of the tamagotchi
   }
 
   eat(food){
@@ -49,4 +49,56 @@ setInterval(tamagotchi.increaseSleepiness, 1800000);
 setInterval(tamagotchi.increaseBoredom, 1800000);
 setInterval(tamagotchi.increaseAge, 1800000);
 
+/* === Set Global Variables for Gameplay === */
+const $eat = $(".gg-bowl");
+const $lightOn = $(".gg-sun");
+const $play = $(".gg-games");
+const $time = $(".gg-sand-clock");  
+//const $("")
+const navBarArray = [$eat, $lightOn, $play, $time];
 
+const selectedTextColor = 'rgb(244, 245, 211)';
+const unselectedTextColor = 'rgb(31, 27, 27)';
+
+/* === Set Actions for Buttons === */
+
+const highlightIcons = function(event){
+  if ($eat.css("color") === unselectedTextColor && $lightOn.css("color") === unselectedTextColor && $play.css("color") === unselectedTextColor && $time.css("color") === unselectedTextColor) {
+    
+    $eat.css("color", "var(--main-text-color)");
+  
+  } else if ($eat.css("color") === selectedTextColor && $lightOn.css("color") === unselectedTextColor && $play.css("color") === unselectedTextColor && $time.css("color") === unselectedTextColor) {
+    
+    $eat.css("color", "var(--secondary-text-color)");
+    $lightOn.css("color", "var(--main-text-color)");
+  
+  } else if ($eat.css("color") === unselectedTextColor && $lightOn.css("color") === selectedTextColor && $play.css("color") === unselectedTextColor && $time.css("color") === unselectedTextColor) {
+    
+    $lightOn.css("color", "var(--secondary-text-color)");
+    $play.css("color", "var(--main-text-color)");
+  
+  } else if ($eat.css("color") === unselectedTextColor && $lightOn.css("color") === unselectedTextColor && $play.css("color") === selectedTextColor && $time.css("color") === unselectedTextColor) {
+    
+    $play.css("color", "var(--secondary-text-color)");
+    $time.css("color", "var(--main-text-color)");
+  
+  } else {
+    
+    $time.css("color", "var(--secondary-text-color)");
+    $eat.css("color", "var(--main-text-color)");
+  
+  }
+
+}
+
+const selectAction = function(event){
+
+}
+
+const cancelAction = function(event){
+
+}
+
+$("#A").click(highlightIcons);
+$("#B").click(selectAction);
+$("#C").click(cancelAction)
